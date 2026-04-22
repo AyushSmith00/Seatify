e5a1032213e020aab438830fe00be1504e365e47
Ayush <ayushsmth@gmail.com>
2026-04-22 19:22:26 +0530

    adding register page

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,11 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use(cookieParser())
-
 app.use("/api/auth", authRoutes)
 app.use("/api/events", eventRoutes)
 app.use("/api/bookings", bookingRoutes)
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,72 @@
-function Register(){
-    return <h1 className="text-2xl">This is Register Page</h1>
+import { useState } from "react";
+import API from "../services/api.js";
+
+function Register() {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+    
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post("/auth/register")
+            alert("Registered Successfully");
+            console.log(res.data);
+
+        } catch (error) {
+            console.error(error)
+            alert("Register Failed");
+        };
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-4 bg-gray-800 p-6 rounded text-white w-80">
+
+                <h2 className="text-xl font-bold">Register</h2>
+
+                <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleChange}
+                className="p-2 rounded text-black"
+                />
+
+                <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                className="p-2 rounded text-black"
+                />
+
+                <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                className="p-2 rounded text-black"
+                />
+
+                <button className="bg-blue-500 p-2 rounded">
+                Register
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default Register
